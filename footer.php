dd14a6cf89913ec64bfd2b9d43da0cfc8f600edc
mpedone <mpedone@gmail.com>
2016-09-24 21:57:16 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <!-- Begin Footer -->
-<div class="container footer">
-		<p><img src="images/bottom_brand.png" style="float:left;"> &copy; Copyright 2016, The Pedone Factor. All rights reserved.</p>
-	</div>
+		</body>
 
-</body>
+		<footer>
+			<div class="container footer">
+				<p><img src="images/bottom_brand.png" style="float:left;"> &copy; Copyright 2016, The Pedone Factor. All rights reserved.</p>
+			</div>
+		</footer>
+
+	
 </html>
