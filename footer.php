aa6c5dbcf2f74b52c66acddd0f04caf3f5405460
mpedone <mpedone@gmail.com>
2016-09-24 21:53:59 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!-- Begin Footer -->
+<div class="container footer">
+		<p><img src="images/bottom_brand.png" style="float:left;"> &copy; Copyright 2016, The Pedone Factor. All rights reserved.</p>
+	</div>
+
+</body>
+</html>
